28a1e5cd6dd74ead134d0222bb7d506ea5d8c27c
ErickSebas <ume0028566@est.univalle.edu>
2023-08-23 12:15:16 -0400

    FinalizadoAdmin

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
-import 'package:admin/services/services_firebase.dart';
+import 'package:admin/Presentation/Screens/SplashScreen.dart';
 import 'package:flutter/material.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:excel/excel.dart';
-import 'dart:io';
-import 'package:xml/xml.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:firebase_core/firebase_core.dart';
 import '../firebase_options.dart';
-import 'package:admin/Models/Ubication.dart';
-import 'Screens/SplashScreen.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
